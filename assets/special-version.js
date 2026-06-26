00dacb4fd13b1fd3573f894eeb5b49fda4dff0c8
Akhmed8251 <bratya_250@mail.ru>
2026-06-26 13:28:46 +0300

    first commit

---
+++ assets/special-version.js
@@ -0,0 +1,3 @@
+var path = window.location.pathname;
+var host = window.location.hostname;
+document.getElementById("specialVersion").href = "https://finevision.ru/?hostname=" + host + "&path=" + path
