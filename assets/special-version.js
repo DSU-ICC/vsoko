fd2a9e4ee9879e22772d0aace5f522c0c1bfa24c
Akhmed8251 <bratya_250@mail.ru>
2026-09-07 14:21:26 +0300

    обновление 07.09

--- assets/special-version.js
+++
@@ -1,3 +0,0 @@
-var path = window.location.pathname;
-var host = window.location.hostname;
-document.getElementById("specialVersion").href = "https://finevision.ru/?hostname=" + host + "&path=" + path
